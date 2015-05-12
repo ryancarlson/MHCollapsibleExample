56c4b6385d2895fbba73d2456c31b33d7ff6ddc4
LizzyRandall <lizzyrandall@Lizzys-MacBook-Pro.local>
2015-05-12 09:31:50 -0400

    Initial Commit

---
+++ CollapsibleExample/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CollapsibleExample
+//
+//  Created by Lizzy Randall on 5/12/15.
+//  Copyright (c) 2015 Lizzy Randall. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
