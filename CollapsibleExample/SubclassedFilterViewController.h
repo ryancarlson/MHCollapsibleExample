b2cfdd56a9431952c49d52c36e06f24c69e357c8
LizzyRandall <lizzyrandall@Lizzys-MacBook-Pro.local>
2015-06-11 13:51:05 -0400

    Moved properties and methods
    I’m trying to find what a subclass can know about its super class. I’m
    assuming the properties and methods need to be public for them in order
    to update variables. After moving these methods I’m going to see if a
    subclass should have access to what it does and if instead I should
    encase a lot of the variables for the filterviewcontroller.

---
+++ CollapsibleExample/SubclassedFilterViewController.h
@@ -0,0 +1,13 @@
+//
+//  SubclassedFilterViewController.h
+//  CollapsibleExample
+//
+//  Created by Lizzy Randall on 6/11/15.
+//  Copyright (c) 2015 Lizzy Randall. All rights reserved.
+//
+
+#import "FilterViewController.h"
+
+@interface SubclassedFilterViewController : FilterViewController
+
+@end
--- FilterViewController.m
+++ FilterViewController.m
@@ -8,58 +8,12 @@
 
 #import "FilterViewController.h"
 
-@interface FilterViewController() 
+@interface FilterViewController()
 
 - (NSArray*)returnLabelArray;
 - (NSArray*)returnAssignedToArray;
 - (NSArray*)returnSurveyArray;
 - (NSArray*)returnSurveyAnswers;
-
-//Initial methods called in viewDidLoad
-- (void)setHalfModalViewLook;
-- (void)createManagersAndPopulateData;
-
-//Modal interaction methods
-- (void)createModalWithType:(CRUCellViewInteractionType)cellType section:(MHCollapsibleSection *)section rowPath:(NSIndexPath *)rowPath;
-- (void)bringUpHalfModalWithController:(UINavigationController*)navigationController cgSize:(CGSize)size;
-- (void)removeHalfModalWithController:(UINavigationController*)navigationController cgSize:(CGSize)size;
-- (void)resignFirstResponderWithClearOption:(BOOL)clear;
-- (IBAction)buttonTapped:(UIBarButtonItem*)sender; //modals and this viewcontroller call
-
-//Modal settings
-- (void)setButtonsAndColorWithController:(UIViewController*)viewController bgColor:(UIColor*)bgColor
-                                  cancel:(UIBarButtonItem*)cancel save:(UIBarButtonItem*)save clear:(UIBarButtonItem*)clear;
-- (void)setSettingsForNavWithController:(UINavigationController*)navigationController cgSize:(CGSize)size;
-
-//Handles clear, save and cancel for modals
-- (void)saveChangesForCurrentSection;
-- (void)cancelChangesForCurrentSection;
-- (void)clearChangesForCurrentSection;
-
-//Creating pieces for modals
-- (UILabel*)createLabelWithSection:(MHCollapsibleSection*)section rowPath:(NSIndexPath*)rowPath cgSize:(CGSize)size;
-- (UITextField*)createTextFieldWithSection:(MHCollapsibleSection*)section cgSize:(CGSize)size;
-
-//Tableview delegate
-- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section;
-- (UITableViewCell*)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath;
-- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath;
-
-//used for picker and textfield type modals
-//since they only take up half a page anyway
-@property UIView *modalOverlay;
-
-//Keeping track of the current section gives the ability to get data
-//and invoke actions from the filterviewcontroller
-@property (strong, nonatomic) MHCollapsibleSection *currentSection;
-@property (nonatomic) CRUCellViewInteractionType currentModalType;
-@property (nonatomic) NSIndexPath *currentRowPath;
-
-//Checked for Clear button since it appears on modals as well
-@property (nonatomic) BOOL modalCurrentlyShown;
-//After save button, this array is populated with MHPakcgaedFilter records
-//returned by each MHCollapsibleViewManager
-@property (nonatomic, strong) NSMutableArray *combinedFilters;
 
 @end
 
