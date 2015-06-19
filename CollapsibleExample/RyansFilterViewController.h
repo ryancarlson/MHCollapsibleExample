6efb3b7953b1cb2cfcce04011fd42f0e5b62f144
ryancarlson <ryan.t.carlson@cru.org>
2015-06-19 14:49:43 -0400

    Add my implementation of FilterViewController

---
+++ CollapsibleExample/RyansFilterViewController.h
@@ -0,0 +1,13 @@
+//
+//  RyansFilterViewController.h
+//  CollapsibleExample
+//
+//  Created by Ryan Carlson on 6/19/15.
+//  Copyright (c) 2015 Lizzy Randall. All rights reserved.
+//
+
+#import "FilterViewController.h"
+
+@interface RyansFilterViewController : FilterViewController
+
+@end
---
+++ CollapsibleExample/RyansFilterViewController.m
@@ -0,0 +1,23 @@
+//
+//  RyansFilterViewController.m
+//  CollapsibleExample
+//
+//  Created by Ryan Carlson on 6/19/15.
+//  Copyright (c) 2015 Lizzy Randall. All rights reserved.
+//
+
+#import "RyansFilterViewController.h"
+
+@interface RyansFilterViewController ()
+
+@end
+
+@implementation RyansFilterViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    
+}
+
+@end
